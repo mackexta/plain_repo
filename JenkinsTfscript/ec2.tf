deacfe808fa6c7df4becfa2c0fd54e1f06b06962
kester emefiena <mackexta@yahoo.com>
2025-03-21 11:11:56 -0500

    new files

--- JenkinsTfscript/ec2.tf
+++ JenkinsTfscript/ec2.tf
@@ -1,15 +1,15 @@
 # configured aws provider with proper credentials
 provider "aws" {
-  region    = "us-east-1"
-  profile   = "kexta"
+  region  = "us-east-1"
+  profile = "kexta"
 }
 
 
 # create default vpc if one does not exit
 resource "aws_default_vpc" "default_vpc" {
 
-  tags    = {
-    Name  = "default vpc"
+  tags = {
+    Name = "default vpc"
   }
 }
 
@@ -22,9 +22,32 @@ data "aws_availability_zones" "available_zones" {}
 resource "aws_default_subnet" "default_az1" {
   availability_zone = data.aws_availability_zones.available_zones.names[0]
 
-  tags   = {
+  tags = {
     Name = "default subnet"
   }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_default_vpc.default_vpc.id
+
+  tags = {
+    Name = "demo_igw"
+  }
+}
+
+# creating route table
+resource "aws_route_table" "example" {
+  vpc_id = aws_default_vpc.default_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+# creating subnet association
+resource "aws_route_table_association" "asso" {
+  subnet_id      = aws_default_subnet.default_az1.id
+  route_table_id = aws_route_table.example.id
 }
 
 
@@ -36,30 +59,30 @@ resource "aws_security_group" "ec2_security_group" {
 
   # allow access on port 8080
   ingress {
-    description      = "http proxy access"
-    from_port        = 8080
-    to_port          = 8080
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "http proxy access"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   # allow access on port 22
   ingress {
-    description      = "ssh access"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "ssh access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = -1
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags   = {
+  tags = {
     Name = "jenkins server security group"
   }
 }
@@ -68,19 +91,19 @@ resource "aws_security_group" "ec2_security_group" {
 # use data source to get a registered amazon linux 2 ami
 data "aws_ami" "ubuntu" {
 
-    most_recent = true
+  most_recent = true
 
-    filter {
-        name   = "name"
-        values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-    }
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
 
-    filter {
-        name = "virtualization-type"
-        values = ["hvm"]
-    }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 
-    owners = ["099720109477"]
+  owners = ["099720109477"]
 }
 
 # launch the ec2 instance
@@ -89,8 +112,8 @@ resource "aws_instance" "ec2_instance" {
   instance_type          = "t2.small"
   subnet_id              = aws_default_subnet.default_az1.id
   vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
-  key_name               = "kristen-kp"
-  # user_data = "${file("install_jenkins.sh")}"
+  key_name               = "kesley"
+  # user_data              = file("install_jenkins.sh")
 
   tags = {
     Name = "jenkins_server"
@@ -105,7 +128,7 @@ resource "null_resource" "name" {
   connection {
     type        = "ssh"
     user        = "ubuntu"
-    private_key = file("~/Downloads/kristen-kp.pem")
+    private_key = file("~/Downloads/kesley.pem")
     host        = aws_instance.ec2_instance.public_ip
   }
 
@@ -118,8 +141,8 @@ resource "null_resource" "name" {
   # set permissions and run the install_jenkins.sh file
   provisioner "remote-exec" {
     inline = [
-        "sudo chmod +x /tmp/install_jenkins.sh",
-        "sh /tmp/install_jenkins.sh",
+      "sudo chmod +x /tmp/install_jenkins.sh",
+      "sh /tmp/install_jenkins.sh",
     ]
   }
 
@@ -130,5 +153,5 @@ resource "null_resource" "name" {
 
 # print the url of the jenkins server
 output "website_url" {
-  value     = join ("", ["http://", aws_instance.ec2_instance.public_dns, ":", "8080"])
+  value = join("", ["http://", aws_instance.ec2_instance.public_dns, ":", "8080"])
 }
